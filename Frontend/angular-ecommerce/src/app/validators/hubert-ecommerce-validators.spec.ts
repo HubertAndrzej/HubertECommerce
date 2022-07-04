c827a1ce902955d0f81fb3b5292166ff7a67d64b
HubertAndrzej <hubertandrzejsobolewski@gmail.com>
2022-07-04 16:10:40 +0200

    Added Checkout Validation

---
+++ Frontend/angular-ecommerce/src/app/validators/hubert-ecommerce-validators.spec.ts
@@ -0,0 +1,7 @@
+import { HubertECommerceValidators } from './hubert-ecommerce-validators';
+
+describe('HubertECommerceValidators', () => {
+  it('should create an instance', () => {
+    expect(new HubertECommerceValidators()).toBeTruthy();
+  });
+});
