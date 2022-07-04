36d495f2b1242cb3dad773969185617c7870851f
HubertAndrzej <hubertandrzejsobolewski@gmail.com>
2022-07-04 19:04:22 +0200

    Created Address, Customer, Order and OrderItem Entities

---
+++ Backend/spring-boot-ecommerce/src/main/java/com/webapp/ecommerce/entities/Address.java
@@ -0,0 +1,36 @@
+package com.webapp.ecommerce.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "address")
+@Data
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "zip_code")
+    private String zipCode;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Order order;
+
+}
