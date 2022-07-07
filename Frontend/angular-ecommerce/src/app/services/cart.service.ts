fd1c15dfb965c7cc534836ab94474b8dd7e2449a
HubertAndrzej <hubertandrzejsobolewski@gmail.com>
2022-07-07 15:40:46 +0200

    Added Storage

--- Frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ Frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -12,7 +12,18 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  constructor() { }
+  storage: Storage = localStorage;
+
+  constructor() {
+
+    let data = JSON.parse(this.storage.getItem('cartItems'));
+
+    if (data != null) {
+      this.cartItems = data;
+    }
+
+    this.computeCartTotals();
+  }
 
   addToCart(theCartItem: CartItem) {
 
@@ -47,6 +58,13 @@ export class CartService {
 
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
+
+    this.persistCartItems();
+  }
+
+  persistCartItems() {
+
+    this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
   }
 
   decrementQuantity(theCartItem: CartItem) {
