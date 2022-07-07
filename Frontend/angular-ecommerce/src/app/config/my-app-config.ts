7e506fec2861acb4680963de5469075faeee90cf
HubertAndrzej <hubertandrzejsobolewski@gmail.com>
2022-07-07 13:46:43 +0200

    Added Okta Login Functionality

---
+++ Frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+  oidc: {
+    clientId: '0oa5p72y7yt5WucCK5d7',
+    issuer: 'https://dev-96407740.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+}
